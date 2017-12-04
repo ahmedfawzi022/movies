3808005f925f171b8bdf4ba663d23d94ffa1f4f2
ahmed fawzi <ahmedfawzi022@gmail.com>
2017-12-05 01:09:56 +0200

    back end

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var app = express()
+ app.get('/', function (req, res) {
+  res.send('Welcome api you can try /movies')
+})
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/movies', function (req, res) {
+  res.sendFile(__dirname+'/public/movies.json')
+})
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+ 
+app.listen(3000)
